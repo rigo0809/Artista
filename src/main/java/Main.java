09f21bf343c70630812715d9367af7ed85a1d776
rigo0809 <169068411+rigo0809@users.noreply.github.com>
2024-05-13 23:26:59 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    System.out.println("********************");
+    System.out.println("*Artista: MC Kevin *");
+    System.out.println("********************");
+    System.out.println(" ");
+    System.out.println("############################################");
+    System.out.println("# Álbum de MC Kevin Passado e Presente #");
+    System.out.println("############################################");
+    System.out.println(" ");
+    System.out.println("Lançamento do Álbum Passado e Presente");
+    System.out.println("21 de maio de 2021");
+    System.out.println(" ");
+    System.out.println("=======================================================");
+
+    System.out.println("Lançado em 21 de maio de 2021, ele já é um sucesso e tem sido aclamado pelos fãs.");
+    System.out.println("======================================================= ");
+    System.out.println(" ");
+    System.out.println("+++++++++++++++++++++++++");
+    System.out.println("+ Minutagem e Músicas   +");
+    System.out.println("+++++++++++++++++++++++++");
+    System.out.println("  ");
+
+    System.out.println("1 - 1 Milhao de Sonhos Minutagem 3:45");
+    System.out.println(" ");
+    System.out.println("2 - Opção Minutagem Minutagem 4:06 ");
+    System.out.println(" ");
+    System.out.println("3 - Trilha Minutagem 2:59");    
+    System.out.println(" ");
+    System.out.println("4 - Vida Longa Minutagem 2:50");
+    System.out.println(" ");
+    System.out.println("5 - Grana Minutagem 3:53");
+    System.out.println(" ");
+    System.out.println("6 - Sistema Minutagenm 3:58");
+    System.out.println(" ");
+    System.out.println("7 - Junção Venenosa Minutagem 4:01");
+    System.out.println(" ");
+    System.out.println("8 - Inspirtação Minutagem 3:04");
+    System.out.println(" ");
+    System.out.println("9 - Alma Pura Minutagem 4:10");
+    System.out.println(" ");
+    System.out.println("10 - Palhaço Minutagem 2:59");
+    System.out.println(" ");
+    System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+  }
+}
